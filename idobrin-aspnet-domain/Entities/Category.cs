30bbcbd87dcdc692292ba2d58ded96eb50b755e2
Ian Dobrincic <ian.dobrincic@gmail.com>
2026-05-14 20:34:58 +0200

    Removed lazy loading on categories' subcategories

--- idobrin-aspnet-domain/Entities/Category.cs
+++ idobrin-aspnet-domain/Entities/Category.cs
@@ -5,6 +5,6 @@ public class Category : Base
     public string Name { get; set; }
     public int? SupercategoryId { get; set; }
     public Category? Supercategory { get; set; } = null!;
-    public virtual IEnumerable<Category?> Subcategories { get; set; } = new List<Category?>();
+    public IEnumerable<Category?> Subcategories { get; set; } = new List<Category?>();
     public virtual IEnumerable<Product?> Products { get; set; } = new List<Product?>();
 }
