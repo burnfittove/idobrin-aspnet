5c11c19877015e0de3158354152ae1d4bba8216a
Ana <ian.dobrincic@gmail.com>
2026-08-21 20:22:49 +0200

    Cart controller now returns all products in a cart

--- idobrin-aspnet-dal/Repositories/CartRepository.cs
+++ idobrin-aspnet-dal/Repositories/CartRepository.cs
@@ -7,11 +7,28 @@ namespace idobrin_aspnet_dal.Repositories;
 
 public class CartRepository(DatabaseContext context) : BaseRepository<Cart>(context), ICartRepository
 {
+    public override async Task<Cart?> ReturnByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return await DbSet.Include(e => e.CartItems)
+            .ThenInclude(e => e.Item)
+            .ThenInclude(e => e.Product)
+            .FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+    }
+
+    public override async Task<IEnumerable<Cart>> ReturnAllAsync(CancellationToken cancellationToken = default)
+    {
+        return await DbSet.Include(e => e.CartItems)
+            .ThenInclude(e => e.Item)
+            .ThenInclude(e => e.Product)
+            .ToListAsync(cancellationToken);
+    }
+
     public async Task<Cart?> ReturnUserCartAsync(int userId, CancellationToken cancellationToken = default)
     {
         return await DbSet
             .Include(x => x.CartItems)
             .ThenInclude(e => e.Item)
+            .ThenInclude(e => e.Product)
             .FirstOrDefaultAsync(e => e.UserId == userId, cancellationToken);
     }
 }
